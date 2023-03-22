d07f9be6f09899eca41f1dbc8359305a1b9c83bf
Sahib Tank <sahib.infynno@gmail.com>
2023-03-22 11:27:06 +0530

    log content

--- src/pages/api/webhooks.js
+++ src/pages/api/webhooks.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
       return res.status(400);
 
     case "POST":
-      console.log(req.body, "content");
+      console.log(JSON.stringify(req.body), "content");
       if (req.body.entry[0]?.changes[0]?.value.messages[0].type !== "text")
         return;
 
